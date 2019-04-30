18523831448480d0da63e3b2e594c9c9e2643303
wangyong1 <wangyong1@yidian-inc.com>
2019-04-30 19:01:49 +0800

    安装文件

--- setup/k8smaster_setup.sh
+++ setup/k8smaster_setup.sh
@@ -210,7 +210,7 @@ function install_nodes()
 {
   sudo ansible ${ANSIBLE_K8S_NODES} -u ${KUBE_USER} -m copy -a "src=k8sworker_setup.sh dest=~/k8sworker_setup.sh" --sudo
   sudo ansible ${ANSIBLE_K8S_NODES} -u ${KUBE_USER} -m copy -a "src=docker-daemon.json dest=~/docker-daemon.json" --sudo
-  sudo ansible ${ANSIBLE_K8S_NODES} -u ${KUBE_USER} -m command -a 'sh ~/k8sworker_setup.sh' --sudo
+  sudo ansible ${ANSIBLE_K8S_NODES} -u ${KUBE_USER} -m command -a 'sh ~/k8sworker_setup.sh -v ${KUBE_VERSION} -r ${DOCKER_REGISTRY}' --sudo
 }
 
 function masters_join()
